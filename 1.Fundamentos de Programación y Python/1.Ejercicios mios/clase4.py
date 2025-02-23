7f79b84f023752c43f1e02450912335766c0af3f
ppcode <josegomerov90@gmail.com>
2025-02-22 22:59:25 -0500

    Cambios clase strings

---
+++ 1.Fundamentos de Programación y Python/1.Ejercicios mios/clase4.py
@@ -0,0 +1,16 @@
+name = "JOSE Miguel    "
+print(name[0])
+print(name[2])
+print(name[4])
+print(name[5])
+
+last_name = "Gomero Vasquez"
+
+print(name + " " + last_name)
+
+print(5 * name)
+print(len(name))
+
+print(name.lower())
+print(name.upper())
+print(name.strip())
---
+++ 1.Fundamentos de Programación y Python/1.Ejercicios mios/error_sintaxis.py
@@ -0,0 +1,8 @@
+saludo = "Hola"
+nombre = "Jose"
+nombre = "Luis"
+
+print(saludo)
+print(nombre)
+
+print(f"{saludo}\n{nombre}")
